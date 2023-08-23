7bf8234e3a71c2c881260fd00302b175731ffffd
JuanBogliacino <juanboglia12@gmail.com>
2023-08-22 21:15:21 -0300

    user-table complete

--- user-table/src/App.jsx
+++ user-table/src/App.jsx
@@ -1,9 +1,152 @@
 import UserTable from "./components/UserTable"
 
+const usersList = [
+  {
+    id: 1,
+    name: "Ana García",
+    email: "ana@example.com",
+    age: 28,
+    country: "España",
+  },
+  {
+    id: 2,
+    name: "John Smith",
+    email: "john@example.com",
+    age: 35,
+    country: "Estados Unidos",
+  },
+  {
+    id: 3,
+    name: "Maria Santos",
+    email: "maria@example.com",
+    age: 22,
+    country: "Brasil",
+  },
+  {
+    id: 4,
+    name: "Li Wei",
+    email: "li@example.com",
+    age: 30,
+    country: "China",
+  },
+  {
+    id: 5,
+    name: "Sven Andersen",
+    email: "sven@example.com",
+    age: 29,
+    country: "Noruega",
+  },
+  {
+    id: 6,
+    name: "Elena Kovalenko",
+    email: "elena@example.com",
+    age: 27,
+    country: "Rusia",
+  },
+  {
+    id: 7,
+    name: "Rafael Martinez",
+    email: "rafael@example.com",
+    age: 31,
+    country: "México",
+  },
+  {
+    id: 8,
+    name: "Fatima Ahmed",
+    email: "fatima@example.com",
+    age: 24,
+    country: "Egipto",
+  },
+  {
+    id: 9,
+    name: "Luis Fernández",
+    email: "luis@example.com",
+    age: 26,
+    country: "Argentina",
+  },
+  {
+    id: 10,
+    name: "Emily Johnson",
+    email: "emily@example.com",
+    age: 28,
+    country: "Canadá",
+  },
+  {
+    id: 11,
+    name: "Marta Rodriguez",
+    email: "marta@example.com",
+    age: 29,
+    country: "España",
+  },
+  {
+    id: 12,
+    name: "Ahmed Ali",
+    email: "ahmed@example.com",
+    age: 33,
+    country: "Egipto",
+  },
+  {
+    id: 13,
+    name: "Sophie Martin",
+    email: "sophie@example.com",
+    age: 31,
+    country: "Francia",
+  },
+  {
+    id: 14,
+    name: "Takashi Tanaka",
+    email: "takashi@example.com",
+    age: 27,
+    country: "Japón",
+  },
+  {
+    id: 15,
+    name: "Isabella Silva",
+    email: "isabella@example.com",
+    age: 26,
+    country: "Brasil",
+  },
+  {
+    id: 16,
+    name: "Erik Johansson",
+    email: "erik@example.com",
+    age: 30,
+    country: "Suecia",
+  },
+  {
+    id: 17,
+    name: "Olivia Baker",
+    email: "olivia@example.com",
+    age: 28,
+    country: "Canadá",
+  },
+  {
+    id: 18,
+    name: "Alessio Ferrari",
+    email: "alessio@example.com",
+    age: 34,
+    country: "Italia",
+  },
+  {
+    id: 19,
+    name: "Hiroshi Nakamura",
+    email: "hiroshi@example.com",
+    age: 32,
+    country: "Japón",
+  },
+  {
+    id: 20,
+    name: "Sophia Gonzalez",
+    email: "sophia@example.com",
+    age: 25,
+    country: "México",
+  }
+];
+
 function App() {
 
   return (
-    <UserTable />
+    <UserTable usersList={usersList} />
   )
 }
 
--- user-table/src/components/UserTable.jsx
+++ user-table/src/components/UserTable.jsx
@@ -1,104 +1,84 @@
 import { useState } from "react";
 
-const usersList = [
-    {
-      id: 1,
-      name: "Ana García",
-      email: "ana@example.com",
-      age: 28,
-      country: "España",
-    },
-    {
-      id: 2,
-      name: "John Smith",
-      email: "john@example.com",
-      age: 35,
-      country: "Estados Unidos",
-    },
-    {
-      id: 3,
-      name: "Maria Santos",
-      email: "maria@example.com",
-      age: 22,
-      country: "Brasil",
-    },
-    {
-      id: 4,
-      name: "Li Wei",
-      email: "li@example.com",
-      age: 30,
-      country: "China",
-    },
-    {
-      id: 5,
-      name: "Sven Andersen",
-      email: "sven@example.com",
-      age: 29,
-      country: "Noruega",
-    },
-  ];
+const UserTable = ({ usersList }) => {
+  const [users, setUsers] = useState(usersList);
+  const [sortColumn, setSortColumn] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
-const UserTable = () => {
-    const [users, setUsers] = useState(usersList)
-    const [inputValue, setInputValue] = useState('')
-
-    const handleDelete = (id) => {
-        const usersFiltered = users.filter(user => user.id !== id)
-        console.log(usersFiltered);
-        setUsers(usersFiltered)
+  const handleSort = (column) => {
+    if (sortColumn === column) {
+      setSortColumn(null);
+    } else {
+      setSortColumn(column);
     }
+  };
 
-    const changeValue = (e) => {
-        if (e.target.value == ' ') return
-        // console.log(e.target.value);
-        setInputValue(e.target.value)
+  const sortedUsers = [...users].sort((a, b) => {
+    if (sortColumn === "country") {
+      return a.country.localeCompare(b.country);
+    } else if (sortColumn === "age") {
+      return a.age - b.age;
+    } else {
+      return a.name.localeCompare(b.name);
     }
+  });
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        filterByName(inputValue)
-    }
+  const filteredUsers = sortedUsers.filter((user) =>
+    user.name.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
 
-    const filterByName = (value) => {
-        const searchUsers = usersList.filter(user => user.name.toLocaleLowerCase().startsWith(value.toLocaleLowerCase()))
-        console.log(searchUsers);
-        setUsers(searchUsers)
-    }
+  const onDeleteUser = (id) => {
+    // Filtrar la lista de usuarios para eliminar al usuario con el nombre dado
+    const updatedUsers = users.filter((user) => user.id !== id);
+    // Actualizar el estado de los usuarios
+    setUsers(updatedUsers);
+  };
 
   return (
-    <>
-    <h1>UserTable</h1>
+    <div className="container">
+      <header className="header">
+        <h2>Tabla de Usuarios</h2>
 
-    <form onSubmit={handleSubmit}>
-        <input onChange={changeValue} value={inputValue} placeholder="buscar nombre..." />
-    </form>
+        <h4>Total de usuarios: {filteredUsers.length}</h4>
 
-    <table>
-    <thead>
-      <tr>
-        <th>Nombre</th>
-        <th>Correo Electrónico</th>
-        <th>Edad</th>
-        <th>País</th>
-        <th>Acciones</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        users.map(user => (
-        <tr key={user.id}>
-          <td>{user.name}</td>
-          <td>{user.email}</td>
-          <td>{user.age}</td>
-          <td>{user.country}</td>
-          <td><button onClick={()=> handleDelete(user.id)}>delete</button></td>
-        </tr>
-        ))
-      }
-    </tbody>
-    </table>
-    </>
-  )
-}
+        <input
+          type="text"
+          placeholder="Buscar por nombre"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </header>
 
-export default UserTable
+      <table>
+        <thead>
+          <tr>
+            <th onClick={() => handleSort("name")} className="pointer">
+              Nombre
+            </th>
+            <th onClick={() => handleSort("age")} className="pointer">
+              Edad
+            </th>
+            <th onClick={() => handleSort("country")} className="pointer">
+              País
+            </th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredUsers.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.age}</td>
+              <td>{user.country}</td>
+              <td>
+                <button onClick={() => onDeleteUser(user.id)}>Eliminar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserTable;
